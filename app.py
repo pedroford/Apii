8b584a51e6df4396eb6982adbf2d82ab860f5c3b
pedroford <86070453+pedroford@users.noreply.github.com>
2024-07-27 20:35:20 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
+
 from gradio_client import Client
 
-# Inicializa o cliente com a URL do aplicativo Gradio
 client = Client("https://dzxcz-manual-mask.hf.space/")
-
-# Faz a previsÃ£o passando a URL da imagem e o nome da API
 result = client.predict(
-    "https://raw.githubusercontent.com/gradio-app/gradio/main/test/test_files/bus.png",  # URL da imagem
-    api_name="/predict"
+				"https://raw.githubusercontent.com/gradio-app/gradio/main/test/test_files/bus.png",	# str (filepath or URL to image) in 'Original' Image component
+				api_name="/predict"
 )
-
-# Imprime o resultado
 print(result)
+Return Type(s)
+
+# str representing output in 'Mask' Image component
