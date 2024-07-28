98357fcf47bc6d091de9243bf907d5c5e540a670
pedroford <86070453+pedroford@users.noreply.github.com>
2024-07-27 21:04:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
-
 from gradio_client import Client
 
 client = Client("https://dzxcz-manual-mask.hf.space/")
 result = client.predict(
-				"https://raw.githubusercontent.com/gradio-app/gradio/main/test/test_files/bus.png",	# str (filepath or URL to image) in 'Original' Image component
-				api_name="/predict"
+    "https://raw.githubusercontent.com/gradio-app/gradio/main/test/test_files/bus.png",  # str (filepath or URL to image) in 'Original' Image component
+    api_name="/predict"
 )
 print(result)
-Return Type(s)
-
-# str representing output in 'Mask' Image component
+# Return Type(s): str representing the prediction result
